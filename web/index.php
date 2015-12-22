9c586860c5ce46ac6698987eae40cdd8015d7dd7
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2015-12-22 17:56:53 +0200

    Add requirement.php

--- web/index.php
+++ web/index.php
@@ -1,1 +1,9 @@
-Our CRM
+<?php
+// Including the Yii framework itself (1)
+require (__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+// Getting the configuration (2)
+$config = require(__DIR__ . '/../config/web.php');
+
+// Making and launching the application immediatelly (3)
+(new yii\web\Application($config))->run();
