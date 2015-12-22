c149ab36be2a366f3eeca5a77896d3693f3f7008
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2015-12-22 11:39:46 +0200

    Add CRM Operators Steps

--- tests/_support/Step/Acceptance/CRMOperatorSteps.php
+++ tests/_support/Step/Acceptance/CRMOperatorSteps.php
@@ -9,18 +9,40 @@ class CRMOperatorSteps extends \AcceptanceTester
         $I->amOnPage('/customers/add');
     }
     
+    function imagineCustomer()
+    {
+        $faker = \Faker\Factory::create();
+        return [
+            'CustomerRecord[name]' => $faker->name,
+            'Customerrecord[birth_date]' => $faker->date('Y-m-d'),
+            'CustomerRecord[notes]' => $faker->sentence(8),
+            'PhoneRecord[number]' => $faker->phoneNumber
+        ];
+    }
+    
     function fillCustomerDataForm()
     {
-        
+        $I = $this;
+        foreach ($fieldsData as $key ){
+            $I->fillField($key, $value);
+        }
     }
     
     function submitCustomerDataForm()
     {
-        
+        $I = $this;
+        $I->click('Submit');
     }
     
     function seeIAmInListCustomersUi()
     {
-        
+        $I = $this;
+        $I->seeCurrentUrlMatches('/customers/');
+    }
+    
+    function amInListCustomersUi()
+    {
+        $I = $this;
+        $I->amOnPage('/customers');
     }
 }
