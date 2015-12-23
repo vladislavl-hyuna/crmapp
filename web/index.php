4db210d4920f0313b7e4c8b30a4a3cd79791e751
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2015-12-23 11:18:42 +0200

    Add error handling

--- web/index.php
+++ web/index.php
@@ -1,6 +1,14 @@
 <?php
+
+// comment out the following line when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
+
 // Including the Yii framework itself (1)
 require (__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+// Displaying errors
+//ini_set('dispaly_errors', true);
 
 // Getting the configuration (2)
 $config = require(__DIR__ . '/../config/web.php');
