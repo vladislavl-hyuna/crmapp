fd7b74c907e3525487b248c3ed306c8896dd5868
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2015-12-23 13:23:16 +0200

    Create Console and DB config, Yii console

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => '\yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=crmapp',
+    'username' => 'root',
+    'password' => 'V@ld1s81',
+    'charset' => 'utf8',
+];
