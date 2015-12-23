2151f52aa532621130c88406138f9b7baa31743f
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2015-12-23 14:27:43 +0200

    Build translation between Yii and Domain Model

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -6,6 +6,42 @@ use yii\web\Controller;
 
 class CustomersController extends Controller
 {
+    private function store(Customer $customer)
+    {
+        $customer_record = new CustomerRecord();
+        
+        $customer_record->name = $customer->name;
+        $customer_record->birth_date = $customer->birth_date->format('Y-m-d');
+        $customer_record->notes = $customer->notes;
+        
+        $customer_record->save();
+        
+        foreach($customer->phones as $phone){
+            $phone_record = new PhoneRecord();
+            
+            $phone_record->number = $phone->number;
+            $phone_record->customer_id = $customer_record->id;
+            
+            $phone_record->save();
+        }
+    }
+    
+    private function makeCustomer(
+        CustomerRecord $customer_record,
+        PhoneRecord $phone_record
+    )
+    {
+        $name = $customer_record->name;
+        $birth_date = new \DateTime($customer_record->birth_date);
+        
+        $customer = new Customer($name, $birth_date);
+        
+        $customer->notes = $customer_record->notes;
+        $customer->phones[] = new Phone($phone_record->number);
+        
+        return $customer;
+    }
+    
     public function actionIndex()
     {
         $records = $this->getRecordsAccordingToQuery();
---
+++ models/customer/CustomerRecord.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\models\customer;
+
+use yii\db\ActiveRecord;
+
+class CustomerRecord extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'customer';
+    }
+    
+    public function rules()
+    {
+        return [
+            ['id', 'number'],
+            ['name', 'string', 'max' => 256],
+            ['birth_date', 'date', 'format' => 'Y-m-d'],
+            ['notes', 'safe']
+        ];
+    }
+}
